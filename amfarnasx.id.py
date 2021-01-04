1c0c98282c1f7a5b2ccd470add9bf3ace9103c45
Amir Farhan Nasrullah <67219421+Amfarnas@users.noreply.github.com>
2021-01-04 14:37:30 +0700

    Create amfarnasx.id.py

---
+++ amfarnasx.id.py
@@ -0,0 +1,41 @@
+import os
+os.system('clear')
+print '\t         SCRIPT BY AMFARNASX.ID\n'
+
+
+print '\tMAU PERKENALAN DIRI LEWAT MEDIA PYTHON xD\n'
+
+print '1.Nama Lengkap\n'
+print '2.Umur\n'
+print '3.Alamat\n'
+print '4.Hobby\n'
+print '5.Cita-Cita\n'
+print
+pilih=input('PILIH MENU:\n')
+
+if pilih== 1:
+	print ('Nama Lengkap\n')
+	print ('Amir Farhan Nasrullah\n')
+	
+elif pilih== 2:
+	print ('Umur\n')
+	print ('16 Tahun\n')
+	
+elif pilih== 3:
+	print ('Alamat\n')
+	print ('Indonesia xD\n')
+	
+elif pilih== 4:
+	print ('Hobby\n')
+	print ('Pentest\n')
+	
+elif pilih== 5:
+	print ('Cita-Cita\n')
+	print ('Pentester Handal\n')
+	
+
+
+
+
+
+
